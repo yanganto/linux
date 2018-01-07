bd0d8b395cbe1e223e611201c22721f401b38126
Poan Yang <poan_yang@trend.com.tw>
2018-01-07 19:41:20 +0800

    modify the linux logo and set as only one logo

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 
+export PATH=$PATH:~/pi_tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/
+
 KERNEL=kernel7
-make ARCH=arm CROSS_COMPILE=~/pi_tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf- bcm2709_defconfig
-make ARCH=arm CROSS_COMPILE=~/pi_tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf- zImage modules dtbs
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2709_defconfig
+#make ARCH=arm CROSS_COMPILE=~/pi_tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf- zImage modules dtbs
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j3
